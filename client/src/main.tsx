2ff60feaaf22f1f2ba411021e3230c1dad1f6300
Michael Wood <michaelwood33311@icloud.com>
2025-03-16 09:29:18 -0400

    added redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import MobileApp from './layouts/mobile/MobileApp';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <MobileApp />
+    <Provider store={store}>
+      <MobileApp />
+    </Provider>
   </React.StrictMode>
 );
