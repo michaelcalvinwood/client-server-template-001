45c344832026873af8ea5ae82b5eb90749f98bf5
Michael Wood <michaelwood33311@icloud.com>
2025-03-19 07:48:14 -0400

    added creating dbPassword

--- ubuntu-24.04/configurationFiles/config.sh
+++ ubuntu-24.04/configurationFiles/config.sh
@@ -1,5 +1,18 @@
-# Create a password using openssl rand -hex 16
+#!/bin/bash
 
-# append dbPassword={password} to server.conf
+# Check if server.conf exists and contains dbPassword
+if [ -f server.conf ] && grep -q "^dbPassword=" server.conf; then
+    echo "dbPassword already exists in server.conf, skipping password creation"
+else
+    # Create a password using openssl rand -hex 16
+    password=$(openssl rand -hex 16)
 
-echo "HERE WE ARE"
+    # If server.conf doesn't exist, create it
+    if [ ! -f server.conf ]; then
+        touch server.conf
+    fi
+
+    # Append dbPassword={password} to server.conf
+    echo "dbPassword=$password" >> server.conf
+    echo "Added new dbPassword to server.conf"
+fi
