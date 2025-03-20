bbed7a9c2d121195f4c7fa2613b4e01aa8be77b2
Michael Wood <michaelwood33311@icloud.com>
2025-03-20 07:59:57 -0400

    added redux event handler

--- client/src/socketService.js
+++ client/src/socketService.js
@@ -3,25 +3,12 @@
 let socket = null;
 let theStore = null;
 
-const dispatch = async (type, payload, store) => {
-    console.log('socket dispatch', type, payload)
-    store.dispatch({
-        type, payload
-    })
-}
-
-const handleTruthfulQaQuestions = async (data, store) => dispatch('truthfulQa/truthfulQaSetQuestions', data, store)
-const handleResponseMessage = async (data, store) => dispatch('app/appAddResponse', data, store);
-const handleCategoryMessage = async (data, store) => dispatch('app/appAddCategoryResponse', data, store);
-
 export const setupTheSocket = (socketio, url, store) => {
     if (socket) return;
     socket = socketio(url);
     theStore = store;
-    socket.on('message', message => {
-        appMerge(store, {message});
-
-        setTimeout(() => appMerge(store, {message: ''}), 1000)
+    socket.on('redux', (payload, type) => {
+        store.dispatch({type, payload})
     });
 
 }
